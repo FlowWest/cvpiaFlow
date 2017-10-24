141cbb3d6901347af18d46442269798fe6678551
thesadie <sgill@flowwest.com>
2017-10-24 13:10:33 -0700

    nodes selected cal sim

--- data-raw/MikeWrightCalSimOct2017/clean_calsim.R
+++ data-raw/MikeWrightCalSimOct2017/clean_calsim.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(stringr)
+library(devtools)
 
 cvpia_nodes <- read_csv('data-raw/MikeWrightCalSimOct2017/cvpia_calsim_nodes.csv', skip = 1)
 need_split <- cvpia_nodes$calsim_habitat_flow %>% str_detect(', ')
@@ -19,3 +20,25 @@ div_nodes <- c(cvpia_nodes$cal_sim_diversion_nodes[!need_split], div_split)
 diversion_nodes <- div_nodes[!is.na(div_nodes)] %>% str_trim('both') %>% str_replace(',', '')
 
 #combine all nodes to select columns
+all_nodes <- c(habitat_nodes, div_flow_nodes, diversion_nodes, 'X2') %>% unique()
+
+pick_columns <- function(file) {
+  temp <- read_csv(paste0('data-raw/MikeWrightCalSimOct2017/', file), skip = 1)
+  ii <- names(temp) %in% all_nodes
+  cleaned <- temp[6:nrow(temp),ii] %>%
+    rename(date = X2) %>%
+    mutate(date = dmy(date))
+  return(cleaned)
+}
+
+file_names <- list.files('data-raw/MikeWrightCalSimOct2017/', '*.csv')[-5]
+
+tt <- map_df(file_names, pick_columns)
+cvpia_calsim <- tt %>%
+  gather(node, flow, -date) %>%
+  filter(!is.na(flow)) %>%
+  spread(node, flow)
+
+write_rds(cvpia_calsim, 'data-raw/MikeWrightCalSimOct2017/cvpia_calsim.rds')
+
+
